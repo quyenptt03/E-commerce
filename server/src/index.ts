5f393a20305d04f13f43a8a3a616612415e841d0
quyenptt03 <quyenptt03@gmail.com>
2024-09-06 14:41:59 +0700

    basic structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,53 @@
-// Import the 'express' module
+import { config } from "dotenv";
 import express from "express";
+
+// others packages
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+//database
+import connectDB from "./db/connect";
+
+// routes
+import { userRouter } from "./routers";
+
+//middlewares
+import errorHandlerMiddleware from "./middlewares/error-handler";
+
+// config env
+config();
 
 const app = express();
 
-const port = 3000;
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript + Node.js + Express!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use("/api/v1/users", userRouter);
+
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.DB_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+export default app;
---
+++ server/src/routers/user.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  createUser,
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  changePassword,
+  changeUserRole,
+  deleteUser,
+} from "../controllers/user";
+import {
+  authenticateUser,
+  authorizePermissions,
+} from "../middlewares/authentication";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin"), getAllUsers)
+  .post(authenticateUser, authorizePermissions("admin"), createUser);
+router.route("/my-profile").get(authenticateUser, showCurrentUser);
+router.route("/update-user").patch(authenticateUser, updateUser);
+router.route("/change-password").patch(authenticateUser, changePassword);
+router
+  .route("/:id")
+  .get(authenticateUser, getSingleUser)
+  .patch(authenticateUser, authorizePermissions("admin"), changeUserRole)
+  .delete(authenticateUser, authorizePermissions("admin"), deleteUser);
+
+export default router;
